1f7ddb4466ec74d251fe083ac7d866bf32934dee
Sufi Ridho Utomo <sufiridhoutomo@student.unmul.ac.id>
2025-10-28 23:39:18 +0800

    APD_2025

---
+++ Testing/test12.py
@@ -0,0 +1,50 @@
+# nama = [["Anto", "Kewer", "Dadang", "Suket", "Eko", "Dono"], ["Bono", "Agung", "Tono", "Heru", "Asep", "Adang"], ["Heri", "Ujang", "Slamet", "Febri", "Gowo", "Klewang"]]
+
+# for i in nama:
+#     for j in i:
+#         print (j)
+
+# praktikum = ["Mahadewa", 200, True, 45.10, "Iwak", ["APD", 25], ("Hytam")]
+# praktikum.insert(2, "Lawud")
+# print(praktikum)
+
+# praktikum = ["Mahadewa", 200, True, 45.10, "Iwak", ["APD", 25], ("Hytam")]
+# praktikum[4] = "gawang"
+# print(praktikum)
+
+# praktikum = ["Mahadewa", 200, True, 45.10, "Iwak", ["APD", 25], ("Hytam")]
+# print(praktikum)
+# ambil = praktikum.pop(4)
+# print(praktikum)
+# print(ambil)
+
+# ListNama = ["Anto", "Kewer", "Dadang", "Suket", "Eko", "Dono"]
+# print(ListNama)
+
+# angka_ganjil = {1, 3, 5, 7, 9}
+
+# for angka in angka_ganjil:
+#     print(angka) 
+
+# angka_ganjil.discard(4)
+# print(angka_ganjil)
+
+# anomali = {
+#     "nama": "Anto",
+#     "umur": 20,
+#     "alamat": "Bandung",
+#     "hobi": ["Mancing", "Berenang", "Ngegame"]
+# }
+
+# for key, value in anomali.items():
+#     print(f"{key}: {value}")
+
+# konologi = {
+#     "nama" : "ilham menghytamkan",
+#     "umur" : 21,
+#     "alamat" : "hytam city",
+#     "hobi": ["menghytamkan", "dihytamkan", "berhytam"]
+# }
+
+# for hytam, awhh in konologi.items():
+#     print(f"{hytam}: {awhh}")
---
+++ Testing/test14.py
@@ -0,0 +1,45 @@
+import os
+os.system('cls')
+
+# def perkenalan():
+#     print("saya hytam")
+# def salam():
+#     print("salam hytam")
+# def hitung(a, b, c):
+#     x = a&b*c
+#     print(x)
+
+# perkenalan()
+# salam()
+# salam()
+# salam()
+# hitung(10, 5, 2)
+
+# def Luas_JajarGenjang(panjang, tinggi):
+#     luas = panjang * tinggi
+#     return luas
+
+# print("Luas JajarGenjang: ", Luas_JajarGenjang(4, 5))
+
+# Nama = "Dono"
+# Matkul = "Perikanan"
+
+# def info():
+#     Nama = "Bono"
+#     Matkul = "Permancingan"
+#     print("Nama: ", Nama)
+#     print("Matkul: ", Matkul)
+
+# info()
+# print("Nama: ", Nama)
+# print("Matkul: ", Matkul)
+
+# def faktorial(n):
+#     if n == 1 or n == 0:
+#         return 1
+#     else:
+#         return n * faktorial(n - 1)
+
+# hasil = faktorial(5)
+# print(f"Hasil dari 5! adalah: {hasil}")
+
